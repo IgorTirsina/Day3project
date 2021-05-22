3b23e3729b7278f0084c0f74e18271f1bfd01cba
Igor Tirsina <igor.tirsina@yahoo.com>
2021-05-22 11:43:32 -0500

    try HARD reset

--- src/test/java/ResetPractice.java
+++ src/test/java/ResetPractice.java
@@ -1,6 +1,10 @@
 public class ResetPractice {
     public static void main(String[] args) {
         System.out.println("file1");
+
+        System.out.println("try to reset HARD way");
+
+        // committing all together
     }
 
 }
