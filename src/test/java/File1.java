d732766c08cdee39798661f9150fed2ffa5edb59
Igor Tirsina <igor.tirsina@yahoo.com>
2021-05-22 11:22:27 -0500

    adding a loop

--- src/test/java/File1.java
+++ src/test/java/File1.java
@@ -3,5 +3,8 @@ public class File1 {
         System.out.println("hello World");
         System.out.println("how is the weather");
         System.out.println("well let see!");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("hello! ");
+        }
     }
 }
